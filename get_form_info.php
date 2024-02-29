38aa1db9cd9a82f7d333e6321df194b7ad2c6ee7
Seth <sethpatrickrobinson@gmail.com>
2024-02-29 17:32:04 -0600

    Idea submission and basic login page

--- get_form_info.php
+++ get_form_info.php
@@ -2,46 +2,71 @@
 $server_name="localhost";
 $username="root";
 $password="";
-$database_name ="innovationhub";
-
-// $conn=mysqli_connect($server_name, $username, $password, $database_name);
-
-
+$database_name ="capstone";
 
 // Check connection
-try
-{
+try {
     $conn = new PDO("mysql:host=$server_name; dbname=$database_name", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if(isset($_POST['save']))
-    {
+    if(isset($_POST['submit'])) {
         $name = $_POST['name'];
         $email = $_POST['email'];
+        $company = $_POST['company'];
+        $idea = $_POST['idea'];
 
-        // Table name is entry_details
-        // Column names are: first_name, last_name, gender, email, mobile
-        $sql_query = "INSERT INTO innovator (InnovatorName, InnovatorEmail)
-        VALUES (:name, :email)";
-
-        $stmt = $conn->prepare($sql_query);
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':email', $email);
-
-        // Execute the query
-        if($stmt->execute())
-        {
-            echo "Innovation Submitted Succesfully!";
+        $check_innovator = "SELECT InnovatorID FROM innovator WHERE InnovatorName=:name AND InnovatorEmail=:email";
+        $innovator_query = $conn->prepare($check_innovator);
+        $innovator_query->bindParam(':name', $name);
+        $innovator_query->bindParam(':email', $email);
+        $innovator_id = NULL;
+        if (!$innovator_query->execute()) {
+            echo "Error on innovator table: " . $innovator_query . $e->getMessage();
+        } else {
+            // if no entry on table, make new entry
+            if ($innovator_query->rowCount() < 1) {
+                $new_innovator = "INSERT INTO innovator (InnovatorName, InnovatorEmail) VALUES (:name, :email)";
+                $insert = $conn->prepare($new_innovator);
+                $insert->bindParam(':name', $name);
+                $insert->bindParam(':email',$email);
+                if (!$insert->execute()) {
+                    echo "Error on innovator insertion: " . $check_innovator;
+                } else {
+                    $innovator_query->execute();
+                }
+            }
+            $innovator_id = $innovator_query->fetchColumn();
         }
-        else
-        {
-            echo "Error: " . $sql . $e->getMessage();
+
+        $check_company = "SELECT CompanyID FROM company WHERE CompanyName=:company";
+        $company_query = $conn->prepare($check_company);
+        $company_query->bindParam(':company', $company);
+        $company_id = NULL;
+        if (!$company_query->execute()) {
+            echo "Error on company table: " . $check_company;
+        } else {
+            $company_id = $company_query->fetchColumn();
+        }
+        
+        $submit = "INSERT INTO idea (IdeaSubmission, InnovatorID, CompanyID)
+        VALUES (:idea, :innovator_id, :company_id)";
+
+        $stmt = $conn->prepare($submit);
+        $stmt->bindParam(':idea', $idea);
+        $stmt->bindParam(':innovator_id', $innovator_id);
+        $stmt->bindParam(':company_id', $company_id);
+        if($stmt->execute()) {
+            echo "Innovation Submitted Succesfully!";
+        } else {
+            echo "Error: " . $submit . $e->getMessage();
+            
         }
     }
-}
-catch(PDOExecption $e)
-{
-        echo "Connection Failec: " . $e->getessage();
+} catch(PDOExecption $e) {
+        echo "Connection Failed: " . $e->getmessage();
+        die();
+} catch (Exception $ex) {
+        echo "Failed: " . $ex->getmessage();
         die();
 }
